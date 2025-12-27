9b55413cc66200089dab770c5f80fbb883c6037f
skvortsov-ivan <ivan.skvortsov@chasacademy.se>
2025-12-27 18:07:26 +0100

    Added a ConsoleUI folder with different menu classes in order to use the application. Also added two json files, one that I will use myself and which will be in the gitignore list. And the other is an example for other users how to create their own appsettings.json

---
+++ src/ConsoleUI/DisplayMenu.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.ConsoleUI
+{
+    public class DisplayMenu
+    {
+        public static int? DisplayAndUseMenu(List<string> menuOptions, string welcomeText, bool simpleMenu)
+        {
+            //Clear console
+            Console.Clear();
+
+            //Default selectedMenuOption is null for error handling
+            int? selectedMenuOption = null;
+
+            //Loop until user made a correct choice
+            while (selectedMenuOption == null)
+            {
+                //Error handling:
+                if (ValidateUserInput(menuOptions, welcomeText) != true)
+                {
+                    return selectedMenuOption;
+                }
+
+                //Putting the console cursor to its origin
+                Console.SetCursorPosition(0, 0);
+
+                //If user chose to make menu without headline
+                if (simpleMenu)
+                {
+                    PositionWelcomeTextAndDeviders(welcomeText);
+                    Console.WriteLine("");
+                }
+
+                //Explaining how to navigate to user
+                Console.WriteLine("Use arrowkeys to navigate. Press Enter to select.\n");
+
+                //Allowing user to access menu
+                selectedMenuOption = MenuChoice(menuOptions, simpleMenu);
+            }
+
+            //Clear console
+            Console.Clear();
+            Console.SetCursorPosition(0, 0);
+
+            //Returning selected menu option
+            return selectedMenuOption;
+        }
+
+        public static int MenuChoice(List<string> menuOptions, bool simpleMenu)
+        {
+            //Adding Exit Menu option, if it doesn't already exists
+            if (!menuOptions.Contains("Exit menu"))
+            {
+                menuOptions.Add("Exit menu");
+            }
+
+            return MenuInput(menuOptions);
+        }
+
+        public static int MenuInput(List<string> menuOptions)
+        {
+            int currentIndex = 1;
+            int menuStartRow = Console.CursorTop;
+
+            while (true)
+            {
+                ClearMenuArea(menuStartRow, menuOptions.Count);
+                Console.SetCursorPosition(0, menuStartRow);
+                HighlightCurrectChoice(menuOptions, currentIndex - 1);
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                // Move selection up
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    if (currentIndex > 1)
+                    {
+                        currentIndex--;
+                    }
+                }
+                // Move selection down
+                else if (key.Key == ConsoleKey.DownArrow)
+                {
+                    if (currentIndex < menuOptions.Count)
+                    {
+                        currentIndex++;
+                    }
+                }
+                // Select current option
+                else if (key.Key == ConsoleKey.Enter)
+                {
+                    if (currentIndex == menuOptions.Count)
+                    {
+                        return 0;
+                    }
+                    return currentIndex;
+                }
+                continue;
+
+            }
+        }
+
+        //Highlights the current menu option
+        public static void HighlightCurrectChoice(List<string> menuOptions, int selectedIndex)
+        {
+            for (int i = 0; i < menuOptions.Count; i++)
+            {
+                if (i == selectedIndex)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                }
+                else
+                {
+                    Console.ResetColor();
+                }
+
+                //Display menu options and highlight current choice
+                if (i == menuOptions.Count - 1)
+                {
+                    Console.WriteLine($"[{0}]: {menuOptions[i]}");
+                }
+                else
+                {
+                    Console.WriteLine($"[{i + 1}]: {menuOptions[i]}");
+                }
+            }
+
+            Console.ResetColor();
+        }
+
+        //Position welcome text, padding and devider
+        public static void PositionWelcomeTextAndDeviders(string welcomeText)
+        {
+            //Calculating padding for the welcome text and applying it
+            int leftPadding = (Console.WindowWidth - welcomeText.Length) / 6;
+            string paddedText = welcomeText.PadLeft(leftPadding + welcomeText.Length);
+
+            //Default devider length
+            int defaultDividerLength = 49;
+
+            //Devider length
+            int dividerLength;
+
+            //Threshhold for default divider length
+            int defaultDividerLengthThreshhold = 35;
+
+            //Scaling divider if the welcome text exceeds default length
+            if (paddedText.Length > defaultDividerLengthThreshhold)
+            {
+                int threshholdDifference = paddedText.Length - defaultDividerLengthThreshhold;
+                dividerLength = defaultDividerLength + threshholdDifference;
+            }
+            else
+            {
+                dividerLength = defaultDividerLength;
+            }
+
+            //Divider string
+            string divider = new string('=', dividerLength);
+
+            //Print welcome text and dividers
+            Console.WriteLine("\n" + divider);
+            Console.WriteLine(paddedText);
+            Console.WriteLine(divider);
+        }
+
+        //Validates user input
+        public static bool ValidateUserInput(List<string> menuOptions, string welcomeText)
+        {
+            //Check if menu options exist or if there are 0 options
+            if (menuOptions == null || menuOptions.Count == 0 || menuOptions.GetType() != typeof(List<string>))
+            {
+                Console.WriteLine("\nThere are no menu options, please include a list of menu options\n");
+                return false;
+            }
+
+            //Check if menu is too big
+            if (menuOptions.Count > 11)
+            {
+                Console.WriteLine("\nMaximum allowed menu options are 8, please make it shorter\n");
+                return false;
+            }
+
+            //Check if welcome text is too long
+            if (welcomeText.Length > Console.WindowWidth)
+            {
+                Console.WriteLine("Welcome text is too long, please make it shorter");
+                return false;
+            }
+            return true;
+        }
+
+        //Clears the menu area. It's used after the user selects a different menu option.
+        public static void ClearMenuArea(int startRow, int lineCount)
+        {
+            for (int i = 0; i < lineCount; i++)
+            {
+                Console.SetCursorPosition(0, startRow + i);
+                Console.Write(new string(' ', Console.WindowWidth));
+            }
+        }
+    }
+}
---
+++ src/ConsoleUI/MainMenu.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.ConsoleUI
+{
+    public static class MainMenu
+    {
+        public enum CrudAction
+        {
+            Create = 1,
+            Read = 2,
+            Update = 3,
+            Delete = 4
+        };
+
+        public enum EntityType
+        {
+            Member = 1,
+            Book = 2,
+        };
+
+        public static void UseMainMenu()
+        {
+            // Main menu options 
+            List<string> menuOptions = new List<string>
+            {
+                "CRUD-operations",
+                "Loan a book",
+                "Return a book",
+            };
+
+            // Crud menu options 
+            List<string> crudMenuOptions = new List<string>
+            {
+                "Create",
+                "Read",
+                "Update",
+                "Delete"
+            };
+
+            // Entity menu options
+            List<string> entityMenuOptions = new List<string>
+            {
+                "Member",
+                "Book",
+            };
+
+
+            while (true)
+            {
+                bool exitMenu = false;
+                int? userInput = DisplayMenu.DisplayAndUseMenu(menuOptions, "Main menu", true);
+                switch (userInput)
+                {
+                    // CRUD
+                    case 1:
+                        while (!exitMenu)
+                        {
+                            int? userCrudInput = DisplayMenu.DisplayAndUseMenu(crudMenuOptions, "Choose CRUD action", false);
+
+                            if (userCrudInput == 0)
+                            {
+                                exitMenu = true;
+                                break;
+                            }
+
+                            CrudAction action = (CrudAction)userCrudInput;
+
+                            int? userEntityInput = DisplayMenu.DisplayAndUseMenu(entityMenuOptions, "Choose entity", false);
+                            if (userEntityInput == null || userEntityInput == 0) continue;
+
+                            EntityType entity = (EntityType)userEntityInput;
+
+                            switch (action)
+                            {
+                                // Create
+                                case CrudAction.Create:
+
+                                    if (entity == EntityType.Member)
+                                    {
+
+                                    }
+                                    else if (entity == EntityType.Book)
+                                    {
+
+                                    }
+                                    Console.WriteLine("Press Enter to continue\n>");
+                                    Console.ReadKey();
+                                    break;
+
+                                // Read
+                                case CrudAction.Read:
+
+                                    if (entity == EntityType.Member)
+                                    {
+
+                                    }
+                                    else if (entity == EntityType.Book)
+                                    {
+
+                                    }
+                                    Console.WriteLine("Press Enter to continue\n>");
+                                    Console.ReadKey();
+                                    break;
+
+                                // Update 
+                                case CrudAction.Update:
+
+                                    if (entity == EntityType.Member)
+                                    {
+
+                                    }
+                                    else if (entity == EntityType.Book)
+                                    {
+
+                                    }
+                                    Console.WriteLine("Press Enter to continue\n>");
+                                    Console.ReadKey();
+                                    break;
+
+                                // Delete
+                                case CrudAction.Delete:
+                                    if (entity == EntityType.Member)
+                                    {
+
+                                    }
+                                    else if (entity == EntityType.Book)
+                                    {
+
+                                    }
+                                    Console.WriteLine("Press Enter to continue\n>");
+                                    Console.ReadKey();
+                                    break;
+                            }
+                        }
+                        break;
+
+                    // Loan a book
+                    case 2:
+                        Console.WriteLine("Press Enter to continue\n>");
+                        Console.ReadLine();
+                        break;
+                    // Return a book
+                    case 3:
+                        Console.WriteLine("Press Enter to continue\n>");
+                        Console.ReadLine();
+                        break;
+                    // Admin exits main menu
+                    case 0:
+                        return;
+                    // Invalid input
+                    default:
+                        Console.WriteLine("\n Invalid input! Please an option between 0–3.\n");
+                        continue;
+                }
+            }
+        }
+    }
+}
+
